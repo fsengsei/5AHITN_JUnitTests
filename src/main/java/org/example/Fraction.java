42539dc97ee8dbee472afb3c1c3d5a8f7c3d2ad5
rhabichl <66315042+rhabichl@users.noreply.github.com>
2022-09-27 20:05:24 +0200

    changed greatest common dividor algorithm for something with better O notation

--- src/main/java/org/example/Fraction.java
+++ src/main/java/org/example/Fraction.java
@@ -119,9 +119,36 @@ public class Fraction {
      * @return the greatest common Divisor
      */
     private int gcd(int n1, int n2) {
+        if (n1 == 0) {
+            return n2;
+        }
+
         if (n2 == 0) {
             return n1;
         }
-        return gcd(n2, n1 % n2);
+
+        int n;
+        for (n = 0; ((n1 | n2) & 1) == 0; n++) {
+            n1 >>= 1;
+            n2 >>= 1;
+        }
+
+        while ((n1 & 1) == 0) {
+            n1 >>= 1;
+        }
+
+        do {
+            while ((n2 & 1) == 0) {
+                n2 >>= 1;
+            }
+
+            if (n1 > n2) {
+                int temp = n1;
+                n1 = n2;
+                n2 = temp;
+            }
+            n2 = (n2 - n1);
+        } while (n2 != 0);
+        return n1 << n;
     }
 }
