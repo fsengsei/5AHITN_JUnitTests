c322c4c5b88911b1dbf67d3d82533a79e3edf566
rhabichl <66315042+rhabichl@users.noreply.github.com>
2022-10-05 09:05:10 +0200

    removed test comment

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,8 +5,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hallo");
-	// test comment
-	int dividend = Integer.parseInt(args[0]);
+        int dividend = Integer.parseInt(args[0]);
         int divisor = Integer.parseInt(args[1]);
 
         Fraction f = new Fraction(dividend, divisor);
